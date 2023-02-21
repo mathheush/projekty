eec1987346f1991a5554305a192db5a500bbd621
mathheush <mateuszkonopka@iclpud.com>
2023-02-21 19:40:19 +0100

    palimdromiczna?

---
+++ palimdromiczna.py
@@ -0,0 +1,13 @@
+Czy palimdromiczna
+a = input("wpisz")
+a = int(a)
+y = 0
+e = a
+while (a > 0):
+    x = a % 10
+    y = (y * 10) + x
+    a = a // 10
+
+if e == y:
+    print("liczba jest palimdromiczna")
+else:print("liczba nie jest palimdromiczna")
