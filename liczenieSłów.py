f62893f800275351823cf2959315c71e80e89b9a
mathheush <mateuszkonopka@iclpud.com>
2023-02-23 11:18:28 +0100

    wersja 1.1 / poprawiono blad przez ktory program zliczal rozne znaki jako slowa

--- liczenieSłów.py
+++ liczenieSłów.py
@@ -1,6 +1,8 @@
 print("Program który liczy ilość słów w tekscie.")
 text = input("Wpisz tekst: ")
 
+text = text.replace(",","").replace(".","").replace("?","").replace("!","").replace("-","")
+
 new = text.split()
 
 print(len(new))
