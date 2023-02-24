b14341bbcb980511fbfe895d349f77ebfebb1a40
mathheush <mateuszkonopka@iclpud.com>
2023-02-24 23:08:50 +0100

    work in progress...

---
+++ kantorWalut.py
@@ -0,0 +1,6 @@
+import requests
+waluta = input("Podaj walutę")
+link = f"http://api.nbp.pl/api/exchangerates/rates/c/{waluta}/"
+print(siema)
+#response = requests.get(link)
+#print(response.json())
