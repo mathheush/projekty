f93a2c829ef2b3f8c2bf43cebaa8e87717928096
mathheush <mateuszkonopka@iclpud.com>
2023-02-25 10:07:03 +0100

    work in progress...

--- kantorWalut.py
+++ kantorWalut.py
@@ -5,10 +5,15 @@ def akcja():
     print("Kantor")
     x = input("Wpisz \"1\", jeśli chcesz sprzedać.\nWpisz \"2\", jeśli chcesz kupić.\nWpisz \"q\", aby zakończyć.\n")
     if x == "1":
+        global do
+        do = "sprzedać"
+        kasa()
         currency()
 
     elif x == "2":
-        pass
+        do = "kupić"
+        kasa()
+        currency()
     elif x == "q":
         pass
     else: print("Nieznana operacja."), akcja()
@@ -20,9 +25,20 @@ def currency():
     global kurs
     waluta = input("Podaj walutę:\n")
     link = requests.get(f'http://api.nbp.pl/api/exchangerates/rates/c/{waluta}/2023-02-24/?format=json')
-    kurs = link.json()
 
+    def jprint(obj):
+        text = json.dumps(obj, sort_keys=True, indent=4)
+        print(text)
+
+    jprint(link.json())
+
+
+
+
+
+
+def kasa():
+    global money
+    money = input(f"Ile chcesz {do}?: ")
 akcja()
 
-#response = requests.get(link)
-#print(response.json())
